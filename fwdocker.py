53d64823e88501014d66e583ab64d54eae973658
John Clayton <johnc@filewave.com>
2016-06-15 15:50:09 +0200

    New command for version.

--- fwdocker.py
+++ fwdocker.py
@@ -9,6 +9,7 @@ VAR_FILEWAVE_VERSION = "FILEWAVE_VERSION"
 VAR_FILEWAVE_DC_FILE = "FILEWAVE_DC_FILE"
 VAR_LAST_COMMAND = "LAST_COMMAND"
 
+
 """
 This docker wrapper uses docker-compose to:
     - make a data volume to store the postgres data, configuration and log files
@@ -33,13 +34,16 @@ if __name__ == "__main__":
                         help="Initialise an all-in-one FileWave MDM Server using docker-compose",
                         action="store_true")
     parser.add_argument("--nosave",
-                        help="dont store the runtime parameters, this is useful in testing or dev environments where you want to use multiple different containers",
+                        help="dont store the runtime parameters, this is useful in testing or dev environments where you want to use multiple different container versions",
                         action="store_true")
     parser.add_argument("--shell",
-                        help="Run a shell within the FileWave MDM Server container",
+                        help="Run a shell within the FileWave MDM Server runtime container",
                         action="store_true")
     parser.add_argument("--data",
-                        help="Run a shell connected to the FileWave MDM data volume (not the MDM server container)",
+                        help="Run a shell within the FileWave MDM Server data container",
+                        action="store_true")
+    parser.add_argument("--version",
+                        help="Print the FileWave MDM Server version to the console, note: this requires the container to be running",
                         action="store_true")
     parser.add_argument("--logs",
                         help="Tail the logs for the FileWave MDM Server container",
@@ -60,7 +64,7 @@ if __name__ == "__main__":
     data_volume_name = "fw_mdm_data"
 
     if not args.init and not args.logs and not args.shell and not args.stop \
-        and not args.start and not args.data and not args.info:
+        and not args.start and not args.info and not args.data and not args.version:
         print "Try using ./fwdocker --init to fire up your first FileWave container!"
         sys.exit(1)
 
@@ -95,31 +99,34 @@ if __name__ == "__main__":
     env[VAR_FILEWAVE_DC_FILE] = dc_file = config.get(APP_SECTION, VAR_FILEWAVE_DC_FILE)
 
     if args.info:
-        print "fwdocker.py Settings"
-        print "===================="
+        print "fwdocker.py Info"
+        print "================"
         print VAR_FILEWAVE_VERSION, ":", env[VAR_FILEWAVE_VERSION]
         print VAR_FILEWAVE_DC_FILE, ":", env[VAR_FILEWAVE_DC_FILE]
         print VAR_LAST_COMMAND, ":", config.get(APP_SECTION, VAR_LAST_COMMAND)
         sys.exit(6)
 
     p = None
+    if args.version:
+        p = "docker exec -it %s /usr/local/sbin/fwxserver -V" % (server_container_name,)
     if args.init:
         p = "docker-compose -f %s -p fw up -d" % (dc_file,)
     if args.logs:
         p = "docker logs -f %s" % (server_container_name,)
     if args.shell:
         p = "docker exec -it %s /bin/bash" % (server_container_name,)
+    if args.data:
+        # this works efficiently because the common base image for both data/runtime volumes is centos:6.6
+        p = "docker run -it --rm --volumes-from %s centos:6.6 /bin/bash" % (data_volume_name,)
     if args.stop:
         p = "docker-compose -f %s -p fw stop" % (dc_file,)
     if args.start:
         p = "docker-compose -f %s -p fw start" % (dc_file,)
-    if args.data:
-        p = "docker run -it --rm --volumes-from %s centos:6.6 /bin/bash" % (data_volume_name,)
 
     config.set(APP_SECTION, VAR_LAST_COMMAND, str(p))
-    with open(settings_path, 'w') as w:
-        config.write(w)
+    if not args.nosave:
+        with open(settings_path, 'w') as w:
+            config.write(w)
 
     if p:
-        print "Command:", p
         call(p.split(), env=env)
