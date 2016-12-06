49854764ba8904a49c3db0236e3cbaee677674aa
Massimo Costa <massimoc@filewave.com>
2016-12-06 14:38:27 +0100

    FW-17615, bumped default version to v12.0.0

--- ucs-server/build.sh
+++ ucs-server/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export FILEWAVE_VERSION=11.2.4
+export FILEWAVE_VERSION=12.0.0
 export LINUX_ZIPFILE_NAME=FileWave_Linux_${FILEWAVE_VERSION}.zip
 
 if [ ! -f $LINUX_ZIPFILE_NAME ]; then
