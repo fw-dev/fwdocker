f85f127e5516a408322f29a7de6f75d48767bf4b
Massimo Costa <massimoc@filewave.com>
2016-12-06 11:42:06 +0100

    FW-17720, restore data from temp folder

--- ucs-server/docker-entrypoint.sh
+++ ucs-server/docker-entrypoint.sh
@@ -1,9 +1,21 @@
 #!/usr/bin/env bash
 
 # first time? empty? you bind-mounted the damn directory - how dare you!
-#if [ ! -d "/fwxserver/DB/pg_data" ]; then
-#    cp -r /usr/local/filewave/tmp/pg_data /fwxserver/DB/
-#fi
+if [ ! -d "/fwxserver/DB" ]; then
+   cp -r /usr/local/filewave/tmp/fwxserver/DB /fwxserver/
+fi
+
+if [ ! -d "/fwxserver/Data Folder" ]; then
+   cp -r /usr/local/filewave/tmp/fwxserver/"Data Folder" /fwxserver/
+fi
+
+if [ ! -d "/usr/local/filewave/certs" ]; then
+   cp -r /usr/local/filewave/tmp/certs /usr/local/filewave/
+fi
+
+if [ ! -d "/usr/local/filewave/apache/conf" ]; then
+   cp -r /usr/local/filewave/tmp/conf /usr/local/filewave/apache
+fi
 
 /usr/local/filewave/python.v27/bin/supervisord -n -c /usr/local/etc/filewave/supervisor/supervisord-server.conf
 
