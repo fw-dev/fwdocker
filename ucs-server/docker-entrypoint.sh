e5f8580425d721942d71458a2e9bce91d3e4466b
Massimo Costa <massimoc@filewave.com>
2016-12-15 11:28:33 +0100

    FW-17617, set supervisord PATH and restore settings_custom.py

--- ucs-server/docker-entrypoint.sh
+++ ucs-server/docker-entrypoint.sh
@@ -1,6 +1,12 @@
 #! /bin/bash
 
-SUPERVISORCTL='/usr/local/filewave/python.v27/bin/supervisorctl -c /usr/local/etc/filewave/supervisor/supervisord-server.conf'
+SUPERVISOR_BASE_PATH="/usr/local/filewave/python.v27/bin"
+if [ ! -f "${SUPERVISOR_BASE_PATH}/supervisord" ]; then 
+    SUPERVISOR_BASE_PATH="/usr/local/filewave/python/bin"
+fi
+echo $"supervisord is located in '${SUPERVISOR_BASE_PATH}''"
+
+SUPERVISORCTL="${SUPERVISOR_BASE_PATH}/supervisorctl -c /usr/local/etc/filewave/supervisor/supervisord-server.conf"
 
 # SIGTERM-handler
 term_handler() {
@@ -34,22 +40,35 @@ fi
 DATA_FOLDER="Data Folder"
 if [ ! -d "/fwxserver/Data Folder" ]; then
     echo $"Restoring Data Folder"
-   cp -r $TEMP_DIR/$DATA_FOLDER /fwxserver/
+    cp -r $TEMP_DIR/$DATA_FOLDER /fwxserver/
 fi
 
 if [ ! "$( ls -A /usr/local/filewave/certs)" ]; then
     echo $"Restoring filewave certs folder"
-   cp -r $TEMP_DIR/certs /usr/local/filewave/
+    cp -r $TEMP_DIR/certs /usr/local/filewave/
 fi
 
 if [ ! "$(ls -A /usr/local/filewave/apache/conf)" ]; then
     echo $"Restoring apache conf folder"
-   cp -r $TEMP_DIR/conf /usr/local/filewave/apache
+    cp -r $TEMP_DIR/conf /usr/local/filewave/apache
 fi
 
 if [ ! "$(ls -A /usr/local/filewave/postgres/conf)" ]; then
     echo $"Restoring postgres conf folder"
-   cp -r $TEMP_DIR/postgres_conf/* /usr/local/filewave/postgres/conf/
+    cp -r $TEMP_DIR/postgres_conf/* /usr/local/filewave/postgres/conf/
+fi
+
+FILEWAVE_TMP_DIR="/usr/local/filewave/tmp"
+if [ -f "$FILEWAVE_TMP_DIR/settings_custom.py" ]; then
+    echo $"Restoring settings_custom file"
+    cp -r $FILEWAVE_TMP_DIR/settings_custom.py /usr/local/filewave/django/filewave
+
+    # Avoid to restore everytime we start
+    if [ -f "$FILEWAVE_TMP_DIR/settings_custom.py.bak" ]; then
+        rm $FILEWAVE_TMP_DIR/settings_custom.py.bak
+    fi
+
+    mv $FILEWAVE_TMP_DIR/settings_custom.py $FILEWAVE_TMP_DIR/settings_custom.py.bak
 fi
 
 # Remove garbage from previous execution
@@ -59,7 +78,7 @@ rm -f /usr/local/filewave/apache/logs/*pid /fwxserver/DB/pg_data/*.pid
 /usr/local/filewave/python/bin/python -m fwcontrol.postgres init_or_upgrade_db_folder
 
 # Run Supervisord in daemon mode
-/usr/local/filewave/python.v27/bin/supervisord -c /usr/local/etc/filewave/supervisor/supervisord-server.conf
+${SUPERVISOR_BASE_PATH}/supervisord -c /usr/local/etc/filewave/supervisor/supervisord-server.conf
 
 echo "Supervisord is running"
 
