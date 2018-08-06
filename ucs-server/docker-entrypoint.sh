e858023fec52992b1cc194cc48f7d0671140270f
Ehsan-Farhadi <ehsan.farhadi@filewave.com>
2018-08-06 12:46:55 +0200

    FW-21885 change db_encryption_key.aes owner

--- ucs-server/docker-entrypoint.sh
+++ ucs-server/docker-entrypoint.sh
@@ -93,7 +93,7 @@ fi
 
 echo $"Restoring owners for file/folders"
 chown root:apache ${FILEWAVE_BASE_DIR}/apache/passwd
-chown apache:apache ${FILEWAVE_BASE_DIR}/certs ${FILEWAVE_BASE_DIR}/certs/server.* ${FILEWAVE_BASE_DIR}/ipa ${FILEWAVE_BASE_DIR}/media ${FILEWAVE_BASE_DIR}/apache/conf ${FILEWAVE_BASE_DIR}/apache/conf/*
+chown apache:apache ${FILEWAVE_BASE_DIR}/certs ${FILEWAVE_BASE_DIR}/certs/server.* ${FILEWAVE_BASE_DIR}/certs/db_symmetric_key*.aes ${FILEWAVE_BASE_DIR}/ipa ${FILEWAVE_BASE_DIR}/media ${FILEWAVE_BASE_DIR}/apache/conf ${FILEWAVE_BASE_DIR}/apache/conf/*
 chown postgres:daemon ${FILEWAVE_BASE_DIR}/certs/postgres.*
 
 # Remove garbage from previous execution
