86c58c2bd10cbda482cd047103de10600f18e1ea
Ehsan Farhadi <37327120+ehsan-farhadi@users.noreply.github.com>
2018-09-10 16:27:55 +0200

    exit docker-entrypoint if check_duplicates failed.

--- ucs-server/docker-entrypoint.sh
+++ ucs-server/docker-entrypoint.sh
@@ -103,7 +103,7 @@ chown postgres:daemon ${FILEWAVE_BASE_DIR}/certs/postgres.*
 rm -f /usr/local/filewave/apache/logs/*pid /fwxserver/DB/pg_data/*.pid
 
 # Check duplicates in the DB
-/usr/local/filewave/python/bin/python -m fwcontrol.postgres check_duplicates
+/usr/local/filewave/python/bin/python -u -m fwcontrol.postgres check_duplicates || exit 1
 
 # Upgrade the cluster DB (if needed) and run migrations
 /usr/local/filewave/python/bin/python -m fwcontrol.postgres init_or_upgrade_db_folder
